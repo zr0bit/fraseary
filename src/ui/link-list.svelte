b38f012913d31e3ae95d3b16a794f7070124262f
zrobit <luismigre47@gmail.com>
2022-07-22 13:54:21 -0500

    update: ui

---
+++ src/ui/link-list.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let items, url, name;
+</script>
+
+<ul>
+	{#each items as item}
+		<li>
+			<a href="{url}{item.slug}">{item[name]}</a>
+		</li>
+	{/each}
+</ul>
+
+<!-- <style>
+	/* ul {
+		list-style: none;
+	} */
+</style> -->
