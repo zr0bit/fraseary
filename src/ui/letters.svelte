f0b5681dfc547c0c2eaa01e40022a94ff00f2b97
zrobit <luismigre47@gmail.com>
2022-07-27 11:28:31 -0500

    Add: letters ui component

---
+++ src/ui/letters.svelte
@@ -0,0 +1,9 @@
+<script>
+	import LinkList from '$ui/link-list.svelte';
+	import { lettersSlug } from '$lib/utils.js';
+	export let baseUrl;
+</script>
+
+<nav class="letters">
+	<LinkList items={lettersSlug} name="name" url={baseUrl} />
+</nav>
