a7cd58ea2000516792f6daa67c7db3ff51a9d17a
zrobit <luismigre47@gmail.com>
2022-07-27 12:31:56 -0500

    Add: queries for tags index

--- src/lib/queries/tag-query.js
+++ src/lib/queries/tag-query.js
@@ -1,4 +1,5 @@
 import Tag from '../models/tag.js';
+import { letters } from '../utils.js';
 
 export function countTags(arg = {}) {
 	return Tag.estimatedDocumentCount(arg).exec();
@@ -10,6 +11,27 @@ export function getTagBySlug(slug) {
 
 export function getPopularTags() {
 	return Tag.find().limit(10).select('name slug').exec();
+}
+
+export function getAllByLetter(letter = 'a', limit = 10) {
+	let regExp = new RegExp(`^${letter}`);
+	let arg = { name: { $regex: regExp, $options: 'i' }, score: { $gte: -1 } };
+
+	return Tag.find(arg)
+		.limit(limit)
+		.sort('name')
+		.select('name slug -_id')
+		.exec();
+}
+
+export function getMainTags(limit = 10) {
+	let letterFns = letters.map((a) => getAllByLetter(a, limit));
+
+	return Promise.all(letterFns).then((tags) => {
+		return letters.map((a, i) => {
+			return { letter: a, tags: tags[i] };
+		});
+	});
 }
 
 // Admin queries
