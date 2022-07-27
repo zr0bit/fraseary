6e9671aeddee0410e159bd905d2b6d24e2440646
zrobit <luismigre47@gmail.com>
2022-07-27 12:33:03 -0500

    Add: task for get data

--- tasks/data.js
+++ tasks/data.js
@@ -2,6 +2,7 @@ import fs from 'fs';
 import gulp from 'gulp';
 import mongoose from 'mongoose';
 import { getMainAuthors } from '../src/lib/queries/author-query.js';
+import { getMainTags } from '../src/lib/queries/tag-query.js';
 import { DEV_DB_CLOUD } from '../src/lib/config.js';
 
 async function connectDB(cb) {
@@ -27,4 +28,16 @@ async function cachedAuthors(cb) {
 	cb();
 }
 
+async function cachedTags(cb) {
+	const tags = await getMainTags(15);
+	try {
+		fs.writeFileSync('./src/routes/tags/_tags.json', JSON.stringify(tags));
+	} catch (err) {
+		console.error(err);
+	}
+	mongoose.connection.close();
+	cb();
+}
+
 gulp.task('get:authors', gulp.series(connectDB, cachedAuthors));
+gulp.task('get:tags', gulp.series(connectDB, cachedTags));
