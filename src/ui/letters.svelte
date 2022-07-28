fa4704002071bdaf56c2e7d5520bb6fec25aafa8
zrobit <luismigre47@gmail.com>
2022-07-28 06:43:30 -0500

    Add: initial styles for letters

--- src/ui/letters.svelte
+++ src/ui/letters.svelte
@@ -7,3 +7,21 @@
 <nav class="letters">
 	<LinkList items={lettersSlug} name="name" url={baseUrl} />
 </nav>
+
+<style>
+	.letters :global(ul) {
+		list-style: none;
+	}
+	.letters :global(li) {
+		list-style: none;
+		text-transform: uppercase;
+		margin-right: 8px;
+	}
+
+	.letters :global(a) {
+		color: red;
+		float: left;
+		margin-right: 8px;
+		text-decoration: none;
+	}
+</style>
