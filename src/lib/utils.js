a784828669b81760dbbbf841e074da0b6642dc52
zrobit <luismigre47@gmail.com>
2022-07-29 21:25:09 -0500

    Fix: letter

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,2 +1,2 @@
-export const letters = 'abcdefghijklbnopqrstuvwxyz'.split('');
+export const letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
 export const lettersSlug = letters.map((a) => ({ name: a, slug: a }));
