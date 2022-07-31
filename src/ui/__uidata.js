7750aa030ab75e0707d1aee06e77b1c46fd3e9f3
zrobit <luismigre47@gmail.com>
2022-07-31 17:13:52 -0500

    Add: data for ui

---
+++ src/ui/__uidata.js
@@ -0,0 +1,45 @@
+export const quotes = [
+	{ name: 'Frases de amor', slug: 'frases-de-amor', icon: 'heart' },
+	{ name: 'Frases cortas', slug: 'frases-cortas', icon: 'lightning' },
+	{
+		name: 'Frases creativas ',
+		slug: 'frases-creativas',
+		icon: 'bulb-light'
+	},
+	{ name: 'Frases graciosas', slug: 'frases-graciosas', icon: 'smile' },
+	{ name: 'Frases inteligentes', slug: 'frases-inteligentes', icon: 'nerd' },
+	{
+		name: 'Frases de motivación',
+		slug: 'frases-de-motivacion',
+		icon: 'arm'
+	},
+	{
+		name: 'Frases de amistad',
+		slug: 'frases-de-amistad',
+		icon: 'friends'
+	},
+	{
+		name: 'Frases de la vida',
+		slug: 'frases-de-la-vida',
+		icon: 'flower'
+	},
+	{ name: 'Frases tristes', slug: 'frases-tristes', icon: 'sad' }
+];
+
+export const authors = [
+	{ name: 'Albert Einstein', slug: 'albert-einstein' },
+	{ name: 'Blaise Pascal', slug: 'blaise-pascal' },
+	{ name: 'Winston Churchill ', slug: 'winston-churchill' },
+	{ name: 'Sócrates', slug: 'socrates' },
+	{ name: 'Isaac Asimov', slug: 'isaac-asimov' },
+	{ name: 'Mario Benedetti', slug: 'mario-benedetti' },
+	{ name: 'Mahatma Gandhi', slug: 'mahatma-gandhi' },
+	{ name: 'George Orwell', slug: 'george-orwell' },
+	{ name: 'Steve Jobs', slug: 'steve-jobs' }
+];
+
+export const social = [
+	{ name: 'facebook', url: 'https://www.facebook.com/fraseary' },
+	{ name: 'twitter', url: 'https://twitter.com/fraseary' },
+	{ name: 'pinterest', url: 'https://www.pinterest.com/fraseary' }
+];
