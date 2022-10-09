46bb211e202cd4ab1c7414829f3b227de7e27076
@zrobit <luismigre47@gmail.com>
2022-10-09 07:03:06 -0500

    add temp for check router update

---
+++ src/routes/temp/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Header from '$ui/header.svelte';
+	import Footer from '$ui/footer/main.svelte';
+</script>
+
+<Header />
+
+<main class="main">
+	<slot />
+</main>
+
+<Footer />
+
+<style>
+	.main {
+		padding-top: 48px;
+	}
+</style>
